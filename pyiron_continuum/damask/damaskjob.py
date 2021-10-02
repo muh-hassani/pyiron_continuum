44777f91a6c5defb0e5f8a7ad74908368bc37aba
muh-hassani <mhassani86@gmail.com>
2021-10-02 03:40:16 +0200

    updating damask job

--- pyiron_continuum/damask/damaskjob.py
+++ pyiron_continuum/damask/damaskjob.py
@@ -1,227 +1,101 @@
-from pyiron_base import GenericJob, DataContainer
+from pyiron_base import TemplateJob, GenericJob, DataContainer
 import numpy as np
 import matplotlib.pyplot as plt
-from damask import Grid
-from damask import Result
-from damask import seeds
-import damask
+from damask import Grid, Result, Config, ConfigMaterial, seeds
 import pyvista as pv
 import h5py
 import os
-import subprocess
+
 
 class DAMASK(GenericJob):
     def __init__(self, project, job_name):
         super(DAMASK, self).__init__(project, job_name)
+        #self.working_directory = os.path.join(project.path, job_name)
+        # if not os.path.exists(self.working_directory):
+        #     os.mkdir(self.working_directory)
         self.input = DataContainer()
         self.output = DataContainer()
-        self._material = None 
+        self._damask_hdf = os.path.join(self.working_directory, "damask_tensionX.hdf5")
+        self.create = Create()
+        self._material = None
         self._loading = None
-        self._geometry = None
-        self._damask_results = None
-        self._damask_cpus=2
-        self.input.create_group('geometry')
-        self.input.create_group('material')
-        self.input.create_group('loading')
-        self.output.create_group('stress')
-        self.output.create_group('strain')
-        #self._executable_activate()
-
-        # set up some default settings
-        self.input.geometry['grains']=4
-        self.input.geometry['size']=1.0e-5 
-        self.input.geometry['grids']=16
-        self.input.geometry['vtifile']=''
-
-        # set up default settings for load
-        self.input.loading['loadfile']=''
-        self.input.loading['time']=0.0
-        self.input.loading['step']=1
-        self.input.loading['interval']=1
-
-        
-    def generatematerialyaml(self,materialphase='Aluminum',homogenizationmethod='SX',c11=106.75e+9,c12=60.41e+9,c44=28.34e+9):
-        r=damask.Rotation.from_random(self.input.geometry['grains'])
-        ### config the material yaml
-        config=damask.ConfigMaterial()
-        config=config.material_add(O=r,phase=materialphase,homogenization=homogenizationmethod)
-        config.save()
-        # for homogenization
-        str='homogenization:\n  %s:\n    N_constituents: 1\n    mechanical: {type: pass}\n'%(homogenizationmethod)
-        fin = open("material.yaml", "rt")
-        data = fin.read()
-        data = data.replace('homogenization: {}',str)
-        # for phase
-        str='phase:\n  %s:\n    lattice: cF\n    mechanical:\n      output: [F, P, F_e, F_p, L_p, O]\n      elastic: {type: Hooke, C_11: %14.5e, C_12: %14.5e, C_44:%14.5e }\n      plastic:\n        type: phenopowerlaw\n        N_sl: [12]\n        a_sl: 2.25\n        atol_xi: 1.0\n        dot_gamma_0_sl: 0.001\n        h_0_sl-sl: 75.e+6\n        h_sl-sl: [1, 1, 1.4, 1.4, 1.4, 1.4, 1.4]\n        n_sl: 20\n        output: [xi_sl]\n        xi_0_sl: [31.e+6]\n        xi_inf_sl: [63.e+6]\n'%(materialphase,c11,c12,c44)
-        data = data.replace('phase: {}',str)
-        fin.close()
-        fin = open("material.yaml", "wt")
-        fin.write(data)
-        fin.close()
-
-        self._material=self.input.material.read('material.yaml')
-    
-
-    def Run(self):
-        str="DAMASK_grid --geom %s --load %s"%(self.input.geometry['vtifile'],self.input.loading['loadfile'])
-        subprocess.run(str,shell=True)
+        self._grid = None
+        self._results = None
+        self._executable_activate()
 
     @property
     def material(self):
         return self._material
-    
+
     @material.setter
-    def material(self, path=None):
-        self._material = self.input.material.read(path)
-        
+    def material(self, value):
+        self._material = value
+
+    @property
+    def grid(self):
+        return self._grid
+
+    @grid.setter
+    def grid(self, value):
+        self._grid = value
+
     @property
     def loading(self):
         return self._loading
-    
+
     @loading.setter
-    def loading(self, path=None):
-        self._loading = self.input.loading.read(path)
-    
-    def loading_write(self):
-        self.input.loading.write('loading.yaml')
-        
-    def material_write(self):
-        self.input.material.write('material.yaml')
-   
-    def generategeometryyaml(self,size=1.0e-5,grains=4,grids=16):
-        self.input.geometry['size']=size
-        self.input.geometry['grains']=grains
-        self.input.geometry['grids']=grids
-        self.input.geometry['vtifile']=''
-        size=np.ones(3)*size
-        cells=[grids,grids,grids]
-        N_grains=grains
+    def loading(self, value):
+        self._loading = value
 
-        # for voronoi seed
-        seeds=damask.seeds.from_random(size,N_grains,cells)
-        new_geom=damask.Grid.from_Voronoi_tessellation(cells,size,seeds)
-        # save our geometry file to a vti file
-        self.input.geometry['vtifile']=f'Polycystal_{N_grains}_{cells[0]}x{cells[1]}x{cells[2]}.vti'
+    def _write_material(self):
+        file_path = os.path.join(self.working_directory, "material.yaml")
+        self._material.save(fname=file_path)
+        self.input.material.read(file_path)
 
-        new_geom.save(f'Polycystal_{N_grains}_{cells[0]}x{cells[1]}x{cells[2]}')
-        
-        self.executable="DAMASK_grid --geom %s --load loading.yaml"%(self.input.geometry['vtifile'])
+    def _write_loading(self):
+        file_path = os.path.join(self.working_directory, "loading.yaml")
+        self._loading.save(file_path)
+        self.input.loading.read(file_path)
 
-    def generateloadyaml(self,loadfilename='tensile.yaml',loadtype='tensile',time=1.0,step=10,interval=2):
-        self.input.loading['loadfile']=loadfilename
-        self.input.loading['time']=time
-        self.input.loading['step']=step
-        self.input.loading['interval']=interval
-        load_case = damask.Config(solver={'mechanical':'spectral_basic'},
-                          loadstep=[])
-        if loadtype=='tensile':
-            dotF = [1.0e-2,0,0, 0,'x',0,  0,0,'x']
-            P = ['x' if i != 'x' else 0 for i in dotF]
+    def _write_geometry(self):
+        file_path = os.path.join(self.working_directory, "damask")
+        self._grid.save(file_path)
+        self.input.geometry.read(file_path)
 
-            load_case['loadstep'].append({'boundary_conditions':{},
-                'discretization':{'t':time,'N':step},'f_out':interval})
-
-            load_case['loadstep'][0]['boundary_conditions']['mechanical'] = \
-                    {'dot_F':[dotF[0:3],dotF[3:6],dotF[6:9]],
-                     'P':[P[0:3],P[3:6],P[6:9]]}
-
-        load_case.save(loadfilename)
-        
-        self.executable="DAMASK_grid --geom %s --load %s"%(self.input.geometry['vtifile'],self.input.loading['loadfile'])
-
-
-    def SaveResult(self):
-        geometryfile=self.input.geometry['vtifile']
-        loadyaml=self.input.loading['loadfile']
-        resultfile=geometryfile[:len(geometryfile)-4]+'_'+loadyaml[:len(loadyaml)-5]+'.hdf5'
-        print('Result is saved to:',resultfile)
-        self.resultfile=resultfile
-        result=damask.Result(self.resultfile)
-
-        # add Cauchy stress
-        result.add_stress_Cauchy()
-        # add strain
-        result.add_strain()
-        # add equivalent von Mises stress
-        result.add_equivalent_Mises('sigma')
-        result.add_equivalent_Mises('epsilon_V^0.0(F)')
-
-        self.epsilon_avg=np.array([np.average(eps,0) for eps in result.get('epsilon_V^0.0(F)_vM').values()])
-
-        self.sigma_avg=np.array([np.average(sig,0) for sig in result.get('sigma_vM').values()])/1.0e6
-        result.export_VTK()
-        self.result=result
-
-    def PlotStressStrain(self):
-        """
-        Plot the stress strain curve from the job file
-        Parameters
-        ----------
-        ax (matplotlib axis /None): axis to plot on (created if None)
-        """
-        ### for stress
-
-        fig, ax = plt.subplots()
-
-        ax.plot(self.epsilon_avg, self.sigma_avg, linestyle='-', linewidth='2.5')
-        ax.grid(True)
-        ax.set_xlabel(r'$\varepsilon_{VM} $', fontsize=18)
-        ax.set_ylabel(r'$\sigma_{VM}$ (MPa)', fontsize=18)
-        return fig, ax
-
-    def geometry_write(self):
-        #seed = seeds.from_random(self.input.geometry['size'], self.input.geometry['grains'])
-        #new_geom = Grid.from_Voronoi_tessellation(self.input.geometry['grids'], self.input.geometry['size'], seed)
-        #seeds=damask.seeds.from_random(size,N_grains,cells)
-        #new_geom=damask.Grid.from_Voronoi_tessellation(cells,size,seeds)
-        i=1
-        #new_geom.save(os.path.join(self.working_directory, "damask"))
-    
     def write_input(self):
         os.chdir(self.working_directory)
-        self.loading_write()
-        self.geometry_write()
-        self.material_write()
-             
+        self._write_loading()
+        self._write_geometry()
+        self._write_material()
+
     def collect_output(self):
-        self.load_results()
-        self.stress()
-        self.strain()
-    
-    def load_results(self, file_name="damask_tensionX.hdf5"):
+        self._load_results()
+        self._stress()
+        self._strain()
+
+    def _load_results(self, file_name="damask_tensionX.hdf5"):
         """
         Open ‘damask_tensionX.hdf5’,add the Mises equivalent of the Cauchy stress, and export it to VTK (file).
         """
-        if self._damask_results is None:
-            self._file_name = os.path.join(self.working_directory, file_name)
-            self._damask_results = Result(self._file_name)
-            self._damask_results.add_stress_Cauchy()
-            self._damask_results.add_strain()
-            self._damask_results.add_equivalent_Mises('sigma')
-            self._damask_results.add_equivalent_Mises('epsilon_V^0.0(F)')
-            self._damask_results.add_calculation('avg_sigma',"np.average(#sigma_vM#)")
-            self._damask_results.add_calculation('avg_epsilon',"np.average(#epsilon_V^0.0(F)_vM#)")
-            self._damask_results.save_VTK(['sigma','epsilon_V^0.0(F)','sigma_vM','epsilon_V^0.0(F)_vM'])
-        return self._damask_results
-    
-    def stress(self):
+        if self._results is None:
+            if file_name != "damask_tensionX.hdf5":
+                self._damask_hdf = os.path.join(self.working_directory, file_name)
+            self._results = Result(self._damask_hdf)
+        return self._results
+
+    def _stress(self):
         """
         return the stress as a numpy array
-        Parameters
-        ----------
-        job_file : str
-          Name of the job_file
         """
-        self.load_results()
-        if self._damask_results is not None:
-            stress_path = self._damask_results.get_dataset_location('avg_sigma')
+        if self._results is not None:
+            stress_path = self._results.get_dataset_location('avg_sigma')
             stress = np.zeros(len(stress_path))
-            hdf = h5py.File(self._damask_results.fname)
-            for count,path in enumerate(stress_path):
+            hdf = h5py.File(self._results.fname)
+            for count, path in enumerate(stress_path):
                 stress[count] = np.array(hdf[path])
             self.output.stress = np.array(stress)/1E6
 
-    def strain(self):
+    def _strain(self):
         """
         return the strain as a numpy array
         Parameters
@@ -229,7 +103,6 @@ class DAMASK(GenericJob):
         job_file : str
           Name of the job_file
         """
-        self.load_results()
         if self._damask_results is not None:
             stress_path = self._damask_results.get_dataset_location('avg_sigma')
             strain = np.zeros(len(stress_path))
@@ -259,4 +132,149 @@ class DAMASK(GenericJob):
         """
         mesh = pv.read(os.path.join(self.working_directory, self._file_name.split('.')[0] + f'_inc0{inc}.vtr'))
         return mesh
-    
+
+    @staticmethod
+    def list_solver():
+        return [{'mechanical': 'spectral_basic'},
+                {'mechanical': 'spectral_polarization'},
+                {'mechanical': 'FEM'}]
+
+
+class Create:
+    def __init__(self):
+        self._grid = GridRefactory()
+        # self._material = MaterialRefactory()
+#        self._loading = LoadingRefactory()
+        # self._homogenization = {}
+
+    @property
+    def grid(self):
+        return self._grid
+
+    @grid.setter
+    def grid(self, value):
+        self._grid = value
+    #
+    # @property
+    # def material(self):
+    #     return self._material
+    #
+    # @material.setter
+    # def material(self, value):
+    #     self._material = value
+    #
+    # @property
+    # def loading(self):
+    #     return self._loading
+    #
+    # @loading.setter
+    # def loading(self, value):
+    #     self._loading = value
+
+    @staticmethod
+    def loading(solver, load_steps):
+        return DamaskLoading(solver=solver, load_steps=load_steps)
+
+    @staticmethod
+    def material(rotation, elements, phase, homogenization):
+        return MaterialRefactory.config(rotation, elements, phase, homogenization)
+
+    @staticmethod
+    def homogenization(method, parameters):
+        return {method: parameters}
+
+    @staticmethod
+    def phase(composition, lattice, output_list, elasticity, plasticity):
+        return {composition: {'lattice': lattice, 'mechanics': {'output': output_list, 'elasticity': elasticity, 'plasticity': plasticity}}}
+
+    @staticmethod
+    def elasticity(**kwargs):
+        _elast = {}
+        for key, val in kwargs.items():
+            _elast[key] = val
+        return _elast
+
+    @staticmethod
+    def plasticity(**kwargs):
+        _plast = {}
+        for key, val in kwargs.items():
+            _plast[key] = val
+        return _plast
+
+    @staticmethod
+    def rotation(method, *args):
+        return method(*args)
+
+class MaterialRefactory:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def config(rotation, elements, phase, homogenization):
+        _config = ConfigMaterial({'material': [], 'phase': phase, 'homogenization': homogenization})
+        for r, e in zip(rotation, elements):
+            _config = _config.material_add(O=r, phase=e, homogenization=list(homogenization.keys())[0])
+        return _config
+
+    @staticmethod
+    def read(self, file_path):
+        return ConfigMaterial.load(fname=file_path)
+
+    @staticmethod
+    def write(self, file_path):
+        ConfigMaterial.save(fname=file_path)
+
+
+class GridRefactory:
+    def __init__(self):
+        self.origin = Grid(material=np.ones((1, 1, 1)), size=[1., 1., 1.])
+
+    @staticmethod
+    def read(file_path):
+        return Grid.load(fname=file_path)
+
+    @staticmethod
+    def via_voronoi_tessellation(grid_dim, num_grains, box_size):
+        if type(grid_dim) == int or type(grid_dim) == float:
+            grid_dim = np.array([grid_dim, grid_dim, grid_dim])
+        if type(box_size) == int or type(box_size) == float:
+            box_size = np.array([box_size, box_size, box_size])
+        seed = seeds.from_random(box_size, num_grains)
+        return Grid.from_Voronoi_tessellation(grid_dim, box_size, seed)
+
+
+class DamaskLoading(Config):
+    def __init__(self, load_steps, solver):
+        super(DamaskLoading, self).__init__(self)
+        self["solver"] = solver
+        if isinstance(load_steps, list):
+            self["loadstep"] = [
+                LoadStep(mech_bc_dict=load_step['mech_bc_dict'],
+                         discretization=load_step['discretization'],
+                         additional_parameters_dict=load_step["additional"])
+                        for load_step in load_steps ]
+        else:
+            self["loadstep"] = [
+                LoadStep(mech_bc_dict=load_steps['mech_bc_dict'],
+                         discretization=load_steps['discretization'],
+                         additional_parameters_dict=load_steps["additional"])
+            ]
+
+
+class LoadStep(dict):
+    def __init__(self, mech_bc_dict, discretization, additional_parameters_dict=None):
+        super(LoadStep, self).__init__(self)
+        self.update({'boundary_conditions': {'mechanical': {}},
+                     'discretization': discretization})
+
+        if additional_parameters_dict is not None and isinstance(additional_parameters_dict, dict):
+            self.update(additional_parameters_dict)
+
+        for key, val in mech_bc_dict.items():
+            self['boundary_conditions']['mechanical'].update({key: LoadStep.load_tensorial(val)})
+
+    @staticmethod
+    def load_tensorial(arr):
+        return [arr[0:3], arr[3:6], arr[6:9]]
+
+
